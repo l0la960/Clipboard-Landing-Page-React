614f9501b1a3eb2fbfb6f2440c89ef527a05cc8f
l0la960 <oyinlolaolaniyan47@gmail.com>
2026-08-31 16:03:57 +0100

    box shadow spread

--- src/App.jsx
+++ src/App.jsx
@@ -130,8 +130,8 @@ function App() {
   <Text heading='Clipboard for IOS and Mac OS' text= "Available for free on the App Store. Download for Mac or IOS, sync with icloud and you're ready to start adding to your clipboard." />
 </div>
    <div className="container_last-section-btns">
-   <Button btnText='Download for IOS' color='hsl(171, 66%, 44%)' boxShadow=" 0px 2px 0px 2px hsl(170, 75%, 35%)"/>
-   <Button btnText='Download for Mac' color='hsl(233, 100%, 69%)' boxShadow="0px 2px 0px 2px hsl(233, 46%, 56%)"/>
+   <Button btnText='Download for IOS' color='hsl(171, 66%, 44%)' boxShadow=" 0px 2px 2px 2px hsl(170, 75%, 35%)"/>
+   <Button btnText='Download for Mac' color='hsl(233, 100%, 69%)' boxShadow="0px 2px 2px 2px hsl(233, 46%, 56%)"/>
    </div>
 
     </div>
